63437a14daa9a03080d639546ff62fbb8f5d7af3
sandyknight <sandyknight90@gmail.com>
2025-01-25 00:03:13 +0000

    initial commit

---
+++ R/get-utla23-data.R
@@ -0,0 +1,15 @@
+library(data.table)
+
+get_utla23_data <- function() {
+
+  uri <- "https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/UTLA_APR_2023_UK_NC/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
+
+  geo <- jsonlite::read_json(uri, simplifyVector = TRUE)
+
+  geo <- data.table::as.data.table(geo[["features"]])
+
+  data.table::setnames(geo, tolower(gsub("properties\\.", "", names(geo))))
+
+  geo[grep("^E", utla23cd, perl = TRUE), .(utla23cd, utla23nm)]
+
+}
